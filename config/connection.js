7b4f8db0be22cea9a0a3751109572c45b7e35521
ferewar <73423237+ferewar@users.noreply.github.com>
2024-04-05 09:41:38 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 let sequelize;
+
 if (process.env.JAWSDB_URL) {
-   sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306,
-  logging: console.log,
-});
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    logging: console.log,
+  });
+}
 
 module.exports = sequelize;
