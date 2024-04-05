1f244de0308801fbc558038260cefbd9c3a78dd6
ferewar <73423237+ferewar@users.noreply.github.com>
2024-04-05 10:03:26 -0700

    add sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,11 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-let sequelize;
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+      logging: console.log,
+    });
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    logging: console.log,
-  });
-}
 
 module.exports = sequelize;
